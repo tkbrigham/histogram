21c3635836ad0628ed52134a4ce598e7a981486b
Thomas Brigham <thomas@thomasbrigham.me>
2016-05-13 21:47:00 -0700

    Working on parsing .html file

--- Main.scala
+++ Main.scala
@@ -3,13 +3,27 @@ import java.io.File
 object Main {
   def main(args: Array[String]): Unit = {
     var mapper = new Mapper
+    var generator = new HtmlGen
 
     var finder = new FileFinder
     var files = finder.recursiveListFiles(new File("."))
 
-    var mapList = mapper.listOfMapsFromFiles(files)
-    var reducedMap = mapper.reduceMaps(mapList)
-    println(reducedMap)
+    generator.sub("sample.html")
+
+    //var mapList = mapper.listOfMapsFromFiles(files)
+    //var reducedMap = mapper.reduceMaps(mapList)
+    //println(reducedMap)
+  }
+
+  class HtmlGen {
+    def sub(file: String) = {
+      var lines = io.Source.fromFile(file).getLines
+      var testing = lines.find(_.contains("{{ array }}"))
+      println(testing)
+      println(testing.getClass)
+      //println(lines.next())
+      //var testing = lines.map(in => if (in 
+    }
   }
 
   class Mapper {
