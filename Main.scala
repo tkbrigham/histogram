85f35fda656b89650ad4df8559af42b674779f25
Thomas Brigham <thomas@thomasbrigham.me>
2016-05-15 16:07:01 -0700

    Check for parameter

--- Main.scala
+++ Main.scala
@@ -10,16 +10,21 @@ import histogram.utils.mapper._
 import histogram.utils.unzipper._
 
 object Main extends App {
-  val finder = new Finder
+  if (args.length == 0) {
+    println("Please provide targetted directory")
+    System.exit(1)
+  } else {
+    val finder = new Finder
 
-  def txtFiles = finder.recursiveListFiles(new File(args(0)))
-  val zipFiles = finder.recursiveListFiles(new File(args(0)), ".zip")
+    def txtFiles = finder.recursiveListFiles(new File(args(0)))
+    val zipFiles = finder.recursiveListFiles(new File(args(0)), ".zip")
 
-  var unzipper = new Unzipper
-  unzipper.unzip(zipFiles)
+    var unzipper = new Unzipper
+    unzipper.unzip(zipFiles)
 
-  var mapper = new Mapper
-  val iterable = mapper.filesToIterable(txtFiles)
-  var generator = new Generator(iterable, "html/TEMPLATE.html")
-  generator.sub
+    var mapper = new Mapper
+    val iterable = mapper.filesToIterable(txtFiles)
+    var generator = new Generator(iterable, "html/TEMPLATE.html")
+    generator.sub
+  }
 }
