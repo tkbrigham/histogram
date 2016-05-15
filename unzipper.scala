2dc7ec0e05594130aaaf1dad3222c3ad8d3cbff3
Thomas Brigham <thomas@thomasbrigham.me>
2016-05-15 15:12:39 -0700

    quiet output of unzipper

--- unzipper.scala
+++ unzipper.scala
@@ -10,7 +10,7 @@ class Unzipper {
       val dir = file.getCanonicalPath.replace(".zip", "")
         // Assumes we don't already have a folder with the name
         // "${zip_file}_unzipped"
-        s"unzip -o $file -d ${dir}_unzipped" !
+        s"unzip -oq $file -d ${dir}_unzipped" !
     }
   }
 }
