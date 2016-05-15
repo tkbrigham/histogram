bdb5215a47d71f5da369a5ff5a47a6808baa28e9
Thomas Brigham <thomas@thomasbrigham.me>
2016-05-15 13:53:02 -0700

    Add unzipper

---
+++ unzipper.scala
@@ -0,0 +1,16 @@
+package unzipper
+
+import java.io._
+import sys.process._
+import scala.language.postfixOps
+
+class Unzipper {
+  def unzip(files: Seq[File]) = {
+    files.foreach { file =>
+      val dir = file.getCanonicalPath.replace(".zip", "")
+        // Assumes we don't already have a folder with the name
+        // "${zip_file}_unzipped"
+        s"unzip -o $file -d ${dir}_unzipped" !
+    }
+  }
+}
